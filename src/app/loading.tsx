0fcf024765149af13ad64debf44afe4ed38c6e56
Michal Kowalczyk <mickowalczyk6@gmail.com>
2024-04-29 12:24:52 +0200

    add home page with note list

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function HomePageLoading() {
+    return (
+      <div className="flex flex-col justify-center items-center w-full h-[100vh]">
+        <h1 className="text-3xl font-bold">Ładowanie listy notatek..</h1>
+      </div>
+    );
+  }
+  
